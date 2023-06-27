bb24ab3236a5344f5957735bec625af5de5def2d
Chethana N P <chethanaprakash.np@gmail.com>
2023-06-27 10:19:56 +0530

    Initial Commit

---
+++ MultipleAPIs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MultipleAPIs
+//
+//  Created by Cumulations Technologies Private Limited on 27/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
